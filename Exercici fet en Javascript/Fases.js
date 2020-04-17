02d394eee58f21b08dcdd21e1112af86ff3f6cc4
Wicka <mesaes@gmail.com>
2020-04-17 13:51:47 +0200

    Ejercicio en Javascript

--- Exercici fet en Javascript/Fases.js
+++ Exercici fet en Javascript/Fases.js
@@ -1,47 +1,45 @@
 
-/*
- * FASE 1
-● Crea una taula (char[]) amb el teu nom on cada posició correspongui a una lletra.
-● Fes un bucle que recorri aquesta taula i mostri per consola cadascuna de les lletres.
- */
+//
+// FASE 1
+//● Crea una taula (char[]) amb el teu nom on cada posició correspongui a una lletra.
+//● Fes un bucle que recorri aquesta taula i mostri per consola cadascuna de les lletres.
+// 
 
 function Fase1() {
 
 	
-        var nom = new Array(6);
-        var nomSenser = new Array(3);
+    let nom = new Array(6);
+    let iCont=nom.length;
 
-		nom [0]='E';
-		nom [1]='s';
-		nom [2]='t';
-		nom [3]='h';
-		nom [4]='e';
-		nom [5]='r';
+	nom [0]='E';
+	nom [1]='s';
+	nom [2]='t';
+	nom [3]='h';
+	nom [4]='e';
+	nom [5]='r';
     
-        nomSenser[0]=nom;
-       
-        for ( let i=0; i<nomSenser.length; i++) {		  //BUCLE PARA RECORRER ARRAY BIDIMENSIONAL
-			
-			for (let j=0; j<nomSenser[i].length; j++) {   //BUCLE PARA RECORRER EL ARRAY GUARDADO EN POSICION i DEL ARRAY BIDIMENSIONAL
-            
-                document.getElementById("resultado").innerHTML+=nomSenser[i][j] +"<br>";
-			}							
-		}
+  
+    for ( let i=0; i<iCont; i++) {		  //BUCLE PARA RECORRER ARRAY BIDIMENSIONAL
+      
+        document.getElementById("resultado").innerHTML+=nom [i] +"<br>";
+	}
 }
 
-/*
-*FASE 2
-●	Canvia la taula per una llista (List<Character>)
-●	Al bucle, si la lletra és una vocal 
-	imprimeix a la consola: ‘VOCAL’. 
-	Sinó, imprimeix: ‘CONSONTANT’.
-●	Si trobes un numero, mostra per pantalla: 
-	‘Els noms de persones no contenen números!’.
-*/
+//
+// FASE 2
+//●	Canvia la taula per una llista (List<Character>)
+//●	Al bucle, si la lletra és una vocal 
+//	imprimeix a la consola: ‘VOCAL’. 
+//	Sinó, imprimeix: ‘CONSONTANT’.
+//●	Si trobes un numero, mostra per pantalla: 
+//	‘Els noms de persones no contenen números!’.
+//
+
+
 
 function Fase2() {
 
-		var nom=['E','2','t','h','e','r']; //DECLARO  LISTA E INICIO MI LISTA 
+		let  nom=['E','2','t','h','e','r']; //DECLARO  LISTA E INICIO MI LISTA 
 		
         for(i=0; i<nom.length; i++ ) {   //RECORRO LISTA POR CADA ELEMENTO Character
             
@@ -68,11 +66,99 @@ function Fase2() {
                     document.getElementById("resultado").innerHTML+="<br>" + "CONSONANT";
 				}
 			}
-		}		
-			
-
+		}	
 }		
 
+//
+// FASE 3
+//●	Emmagatzemar en un Map tant les lletres de la llista 
+//   com el nombre de vegades que apareixen
+//
 
 
+
+function Fase3() {
+
+	
+		//DECLARACION ARRAYLIST		
+        let nom= ['E','s','t','h','e','r'];
+        let cognom1=['M','e','s','a'];
+        let cognom2=['P','a','r','e','j','a']; 
+        //DECLARACION DE UN ARRAY DE ARRAYS		
+        let nombreEntero=[nom,cognom1,cognom2];
+        let i,j,iCont2,iCont1,car;
+        
+        iCont1=nombreEntero.length;
+        
+        for (i=0; i<iCont1; i++){
+
+            iCont2=nombreEntero[i].length;
+            
+            for (j=0; j<iCont2;j++){
+                car=nombreEntero[i][j];
+                car=car.toLowerCase();
+            }
+        }
+
+        document.getElementById("resultado").innerHTML+="<br>" + nombreEntero;
+	
+}
+
+
+//FASE 4
+//●	Crea una altra llista amb el teu cognom on cada posició 
+//	correspongui a una lletra.
+//●	Fusiona les dues llistes en una sola. 
+//	A més, afegeix una posició amb un espai buit 
+//	entre la primera i la segona. És a dir, 
+//	partim de la llista name i surname i 
+//	al acabar l’execució només tindrem una que es dirà fullName.
+//	FullName: [‘N’, ‘A’, ‘M’, ‘E’, ‘ ‘, ‘S’, ‘U’, ‘R‘, ‘N’, ‘A’, ‘M’, ‘E’]
+//
+
+
+function Fase4() {
+
+		
+		//DECLARO TES LISTAS DIFERENTES
+		
+        let nom=[], cognom=[], fullname=[];
+		
+		nom.push('E');		//RELLENO LA LISTA CON EL NOMBRE
+		nom.push('s');
+		nom.push('t');
+		nom.push('h');
+		nom.push('e');
+		nom.push('r');
+				
+		cognom.push('M');	//RELLENOS OTRA LISTA CON EL APELLIDO
+		cognom.push('e');
+		cognom.push('s');
+		cognom.push('a');
+        
+        let car,i,iCont;
+        iCont=nom.length;
+
+		for (i=0; i<iCont; i++) {			//POR CADA ELEMENTO DE LA LISTA NOM REPETIRE EL BUCLE
+           
+            car=nom[i];
+            car=car.toUpperCase(); //PASO A MAYUSCULAS LOS ELEMENTOS 
+			fullname.push(car);				//AÑADO A LA LISTA FINAL LOS ELEMENTOS DE LA LISTA NOM			
+		}
+		
+		fullname.push(' ');			//AÑADO UN ESPACIO A LA LISTA FINAL
+        
+        iCont=cognom.length;
+
+		for (i=0; i<iCont; i++) {		//RECORRO AHORA LA OTRA LISTA COGNOM
+            car=cognom[i];
+            car=car.toUpperCase();	//IDEM A LO ANTERIOR PASO A MAYUSCULAS
+			fullname.push(car);				//IDEM AÑADO A LA LISTA FINAL
+		}
+		
+        document.getElementById("resultado").innerHTML+="<br>" + fullname;
+        //MUESTRO EN CONSOLA LA LISTA FINAL
+	
+	
+}
 
